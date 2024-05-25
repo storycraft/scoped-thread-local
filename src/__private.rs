8500be7410c4a8de8132b218a2f24c15cc233cf7
storycraft <storycraft@pancake.sh>
2024-05-25 20:00:14 +0900

    feat: move common parts, remove scopeguard re export

--- src/__private.rs
+++ src/__private.rs
@@ -1,4 +1,4 @@
-pub use scopeguard;
+use std::{cell::Cell, thread::LocalKey};
 
 pub const EMPTY_MESSAGE: &str = "expected thread local value, found none";
 
@@ -28,19 +28,13 @@ macro_rules! generate {
                 /// Take value and insert into this scoped thread local storage slot for a duration of a closure.
                 ///
                 /// Upon return, this function will restore the previous value and return the taken value.
+                #[inline(always)]
                 pub fn set<$($lt),*>(&self, value: $hkt_ty, f: impl FnOnce()) -> $hkt_ty {
                     INNER.with(|inner| {
                         // SAFETY: extended lifetimes are not exposed and only accessible via higher kinded closure
                         let slot = ::core::cell::Cell::new(Some(unsafe { ::core::mem::transmute(value) }));
 
-                        inner.swap(&slot);
-                        {
-                            $crate::__private::scopeguard::defer! {
-                                inner.swap(&slot);
-                            };
-
-                            f();
-                        }
+                        $crate::__private::with_swapped(inner, &slot, f);
 
                         // SAFETY: restore lifetimes
                         unsafe { ::core::mem::transmute(slot.into_inner().unwrap()) }
@@ -52,29 +46,35 @@ macro_rules! generate {
                 ///
                 /// # Panics
                 /// Panics if set has not previously been called or value is already taken out by parent scope.
+                #[inline(always)]
                 pub fn with<R>(self, f: impl for<$($lt),*> FnOnce(&mut $hkt_ty) -> R) -> R {
-                    INNER.with(|inner| {
-                        let mut val = $crate::__private::scopeguard::guard(
-                            inner.take().expect($crate::__private::EMPTY_MESSAGE),
-                            |val| inner.set(Some(val)),
-                        );
-
-                        f(&mut *val)
-                    })
+                    $crate::__private::with_key(&INNER, |opt| f(opt.as_mut().expect($crate::__private::EMPTY_MESSAGE)))
                 }
 
                 /// Test whether this TLS key has been set for the current thread.
+                #[inline(always)]
                 pub fn is_set(self) -> bool {
-                    INNER.with(|inner| {
-                        $crate::__private::scopeguard::guard(
-                            inner.take(),
-                            |opt| inner.set(opt),
-                        ).is_some()
-                    })
+                    $crate::__private::with_key(&INNER, |opt| opt.is_some())
                 }
             }
         };
     };
+}
+
+pub fn with_swapped<T>(cell1: &Cell<T>, cell2: &Cell<T>, f: impl FnOnce()) {
+    cell1.swap(cell2);
+    scopeguard::defer! {
+        cell1.swap(cell2);
+    };
+
+    f()
+}
+
+pub fn with_key<T, R>(
+    key: &'static LocalKey<Cell<Option<T>>>,
+    f: impl FnOnce(&mut Option<T>) -> R,
+) -> R {
+    key.with(|inner| f(&mut scopeguard::guard(inner.take(), |opt| inner.set(opt))))
 }
 
 #[macro_export]
